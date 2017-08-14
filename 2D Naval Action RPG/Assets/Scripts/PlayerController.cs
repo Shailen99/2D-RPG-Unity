d949da24d5f56b6adda32a0e95cfa0937515f858
Shailen Sheth <sheths@asbindia.org>
2017-08-14 11:25:07 +0530

    Day 4
    - Added Collisions physics in the game
    - Created LoadNewArea Script although it doesn't work
    - Added passive npc & llama npc on island

--- 2D Naval Action RPG/Assets/Scripts/PlayerController.cs
+++ 2D Naval Action RPG/Assets/Scripts/PlayerController.cs
@@ -6,24 +6,33 @@ public class PlayerController : MonoBehaviour {
 
 
 	public float moveSpeed;
-
+	private Rigidbody2D myRigidBody;
 	// Use this for initialization
-	void Start () {
-
+	void Start () 
+	{
+		myRigidBody = GetComponent<Rigidbody2D>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f )
+		if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
 		{
-			transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f ));
-
+			//transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+			myRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidBody.velocity.y);
+		}
+		else
+		{
+			myRigidBody.velocity = new Vector2(0f, myRigidBody.velocity.y);
 		}
 		if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
 		{
-			transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-
+			//transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+			myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
 		}
+		else
+		{
+			myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, 0f);
+		}﻿
 	}
 
 }
---
+++ 2D Naval Action RPG/Assets/Scripts/VillagerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillagerController : MonoBehaviour {
+
+	public float moveSpeed;
+
+	private Rigidbody2D myRigidbody;
+
+	private bool moving;
+
+	public float timeBetweenMove;
+	private float timebetweenMoveCounter;
+	public float timeToMove;
+	private float timeToMoveCounter;
+
+	private Vector3 movedirection;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D> ();
+
+		timebetweenMoveCounter = timeBetweenMove;
+		timeToMoveCounter = timeToMove;
+
+	} 
+
+	// Update is called once per frame
+	void Update () {
+
+		if (moving) 
+		{
+			timeToMoveCounter -= Time.deltaTime;
+			myRigidbody.velocity = movedirection;
+
+			if (timeToMoveCounter < 0f) 
+			{
+				moving = false;
+				timebetweenMoveCounter = timeBetweenMove; 
+			}
+
+
+		} else {
+			timebetweenMoveCounter -= Time.deltaTime;
+			myRigidbody.velocity = Vector2.zero;
+
+			if (timebetweenMoveCounter < 0f) 
+
+				moving = true;
+			timeToMoveCounter = timeToMove;
+
+			movedirection = new Vector3(Random.Range (-1f, 1f) * moveSpeed, Random.Range (-1f, 1f) * moveSpeed, 0f);
+		}
+	}
+}
