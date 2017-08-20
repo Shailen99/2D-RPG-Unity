90cb8e76f425fb4a78c987e808430a792497dbc7
Shailen Sheth <sheths@asbindia.org>
2017-08-20 16:41:59 +0530

    Day 8 mini update
    -Added Blend Tree Animation for Player on Island
    - Added Island

--- Naval Ship Project/Assets/Scripts/PlayerController.cs
+++ Naval Ship Project/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour {
 	public float moveSpeed;
 
 	private Animator anim;
+	private bool playerMoving;
+	private Vector2 LastMove;
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
@@ -14,18 +16,26 @@ public class PlayerController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		playerMoving = false;
+
 		if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f )
 		{
 			transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f ));
+			playerMoving = true;
+			LastMove = new Vector2 (Input.GetAxisRaw ("Horizontal"),0f);
 
 		}
 		if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
 		{
 			transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-
+			playerMoving = true;
+			LastMove = new Vector2 (0f, Input.GetAxisRaw("Vertical"));
 		}
 		anim.SetFloat("Move X", Input.GetAxisRaw("Horizontal"));
 		anim.SetFloat("Move Y", Input.GetAxisRaw("Vertical"));
+		anim.SetBool ("PlayerMoving", playerMoving);
+		anim.SetFloat ("LastMoveX", LastMove.x);
+		anim.SetFloat ("LastMoveY", LastMove.y);
 
 	}
 		
