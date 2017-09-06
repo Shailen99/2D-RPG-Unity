316023b59097b86213acf665ca8ee0134d04a157
Shailen Sheth <sheths@asbindia.org>
2017-09-06 12:25:03 +0530

    Day 14
    - Added Dungeon Area
    - Added transition between beach and dungeon
    - Added sword trigger for game and sword weapon which is a child of the player
    - Added first slime enemy which moves around
    - Added gate trigger which traps the slime and the player within a small area.
    - Began development of maze area for unity game.

---
+++ Naval Ship Project/Assets/Scripts/GateTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateTrigger : MonoBehaviour {
+
+	public GameObject Gate;
+	public GameObject Gtrigger;
+
+	public bool GateActive;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (GateActive && other.gameObject.name == "Player") {
+
+			Gtrigger.SetActive (false);
+			GateActive = true;
+			Gate.SetActive (true);
+		}
+	}
+
+}
