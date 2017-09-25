f74063ca20496071a5f3073b7ee3734e1033bd91
Shailen Sheth <sheths@asbindia.org>
2017-09-25 11:30:05 +0530

    Day 18 update
    - Added key and gate mechanism for the dungeon area
    - Edited the dungeon

---
+++ Naval Ship Project/Assets/Scripts/KeyActivator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyActivator : MonoBehaviour {
+
+	public GameObject key;
+	public bool keyActive;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (keyActive && other.gameObject.name == "Player") {
+			key.SetActive (true);
+			keyActive = false;
+		}
+	}
+
+}
