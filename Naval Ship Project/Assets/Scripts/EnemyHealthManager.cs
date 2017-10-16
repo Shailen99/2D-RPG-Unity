5301a3b3349c1fb768ee2f4392edd934c65d3bc2
Shailen Sheth <sheths@asbindia.org>
2017-10-16 12:24:24 +0530

    Day 24-6
    - Added Leveling UP/Experience System
    - Added Levelling Up to payer Gui
    - Finished Maze

--- Naval Ship Project/Assets/Scripts/EnemyHealthManager.cs
+++ Naval Ship Project/Assets/Scripts/EnemyHealthManager.cs
@@ -7,15 +7,19 @@ public class EnemyHealthManager : MonoBehaviour {
 	public int MaxHealth;
 	public int CurrentHealth;
 
+	private PlayerStats thePlayerStats;
+	public int expToGive;
 	// Use this for initialization
 	void Start () {
 		MaxHealth = CurrentHealth;
+		thePlayerStats = FindObjectOfType<PlayerStats> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (CurrentHealth < 0) {
 			Destroy (gameObject);
+			thePlayerStats.AddExperience (expToGive);
 		}
 	}
 	public void HurtEnemy(int damageToGive)
--- Naval Ship Project/Assets/Scripts/UIManager.cs
+++ Naval Ship Project/Assets/Scripts/UIManager.cs
@@ -8,9 +8,18 @@ public class UIManager : MonoBehaviour {
 	public Slider healthBar;
 	public Text HPText;
 	public PlayerHealthSystem playerHealth;
+	private PlayerStats thePS;
+	public Text levelText;
+	private static bool UIExists;
 	// Use this for initialization
 	void Start () {
-		
+		if (!UIExists) {
+			UIExists = true;
+			DontDestroyOnLoad (transform.gameObject);
+		} else {
+			Destroy (gameObject);
+		}
+		thePS = GetComponent<PlayerStats>();
 	}
 	
 	// Update is called once per frame
@@ -18,5 +27,6 @@ public class UIManager : MonoBehaviour {
 		healthBar.maxValue = playerHealth.playerMaxHealth;
 		healthBar.value = playerHealth.playerCurrentHealth;
 		HPText.text = "HP: " + playerHealth.playerCurrentHealth + "/" + playerHealth.playerMaxHealth;
+		levelText.text = "Lvl: " + thePS.currentLevel;
 	}
 }
