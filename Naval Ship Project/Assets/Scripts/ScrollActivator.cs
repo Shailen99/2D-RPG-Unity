433273d3f89480f5a990682bdd95861de361e76d
Shailen Sheth <sheths@asbindia.org>
2017-10-24 10:59:25 +0530

    Day 29
    - Added Improved Enemy movement
    - Removed sword from movement within the game and only appears when attacking
    - Added a book with lore within it.

---
+++ Naval Ship Project/Assets/Scripts/ScrollActivator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScrollActivator : MonoBehaviour {
+
+	public GameObject OldScroll;
+
+	public bool scrollActive;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (scrollActive && Input.GetKeyDown (KeyCode.Space))
+			{
+			OldScroll.SetActive (false);
+			}
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.name == "Player") {
+			OldScroll.SetActive (true);
+			scrollActive = true;
+		}
+	}
+
+}
