699286d9e2992ca31100a8800a0c71fda0abd309
Shailen Sheth <sheths@asbindia.org>
2017-10-30 15:27:35 +0530

    Day 30
    - Added A tree which can be cut down
    - Added tree cutting mechanic.

---
+++ Naval Ship Project/Assets/CUttingTree.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CUttingTree : MonoBehaviour {
+
+	public GameObject tree;
+	public GameObject wood;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Tree") {
+			tree.SetActive (false);
+			wood.setActive (true);
+		}
+
+}
