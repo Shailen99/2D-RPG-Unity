f02421ddc5407e7a49e32982e600453234daec48
Shailen Sheth <sheths@asbindia.org>
2017-12-07 19:03:23 +0530

    Day 33 mini update 2
    - Removed Skeletons from final room.
    - Edited Final Room to include bouncing spikes for the player to dodge
    - ERROR: Spikes Broken.
    - Spikes given damage so that they're a legitimate puzzle.
    - Added gate unlocked by key from turtle room.

--- Naval Ship Project/Assets/Scripts/HurtPlayer.cs
+++ Naval Ship Project/Assets/Scripts/HurtPlayer.cs
@@ -7,15 +7,15 @@ public class HurtPlayer : MonoBehaviour {
 	public int damageToGive;
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 	void OnCollisionEnter2D(Collision2D other)
-	{ 
+	{
 		if (other.gameObject.name == "Player") {
 			other.gameObject.GetComponent<PlayerHealthSystem>().PlayerHurt(damageToGive);
 		}
