eee5efacf5bbb2f11079b85191d07e415433f69d
Shailen Sheth <sheths@asbindia.org>
2018-01-06 01:20:31 -0500

    Day 42
    - Fixed InGame Sorting Layers for the Overworld
    - Added Coconut Enemy in Bush Hallway.
    - Added Chase AI and Patrol AI to Coconut Enemy. Also Health and Xp system to Enemy.
    - Added Additional Rock Wall in unfinished catacombs area.
    - Began addition of Dialogue For Old Man. Need to Finish.

---
+++ Naval Ship Project/Assets/Scripts/MerchantDialog.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MerchantDialog : MonoBehaviour {
+
+public GameObject dialogBox;
+public GameObject dialogBox2;
+public bool dialogActive;
+public bool dialogSecondBoxActive;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Dialog Box 1
+if(dialogActive && Input.GetKeyDown(KeyCode.Y))
+{
+	dialogBox.SetActive(false);
+	dialogActive = false;
+	dialogBox2.SetActive(true);
+	dialogSecondBoxActive = true;
+}
+if(dialogActive && Input.GetKeyDown(KeyCode.N))
+{
+	dialogBox.SetActive(false);
+	dialogActive = false;
+	dialogSecondBoxActive = false;
+}
+//Dialog Box 2
+if(dialogSecondBoxActive && Input.GetKeyDown(KeyCode.Space))
+{
+	dialogBox2.SetActive(false);
+	dialogSecondBoxActive = false;
+
+}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.name == "Player")
+			{
+			dialogBox.SetActive(true);
+			dialogActive = true;
+			}
+		}
+	}
